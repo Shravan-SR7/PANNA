1d91e94398ea2d0c1897435e7b5f670c4c85f0f3
shravan7777 <balasubramanianshravan@gmail.com>
2020-04-04 16:23:37 +0530

    message

--- mongodb-user-login-master/login_example.py
+++ mongodb-user-login-master/login_example.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, url_for, request, session, redirect
 from pymongo import MongoClient
 import bcrypt
 import re
-from flask import flash
+import pytz
+from datetime import datetime
+from datetime import timedelta
+
 
 app=Flask(__name__)
 
@@ -10,6 +13,7 @@ app.config['MONGO_DBNAME']='mongologin'
 app.config['MONGO_URI']='localhost:27017'
 
 client = MongoClient()
+client.db.temp.drop()
 
 @app.route('/',methods=['GET'])
 def start():
@@ -31,7 +35,8 @@ def logout():
 @app.route('/valid_user',methods = ['POST', 'GET'])
 def valid_user(): 
     if request.method == 'POST':
-        return render_template('logout.html')
+        s1="logged in as " + session['username'] 
+        return render_template("valid_user.html",s=s1)
     if request.method == 'GET':
         s1="logged in as " + session['username'] 
         return render_template("valid_user.html",s=s1)
@@ -78,9 +83,11 @@ def register():
         if existing_user is None :
             s=request.form['pass']
             u=request.form['username']
+            address=request.form['address']
+            email= request.form['email']
             if validate(s,u):
                 hashpass = request.form['pass']
-                users.insert({'name':request.form['username'] , 'password': hashpass})
+                users.insert({'name':request.form['username'] , 'password': hashpass , 'credits':1000, 'address':address, 'email':email})
                 s1=request.form['username'] + " has succesfully been registered"
                 return render_template('index.html',send=s1)
             else:
@@ -97,6 +104,108 @@ def register():
         return 'Username aldready exists'    
     return render_template('register.html')
 
+@app.route('/credit', methods = ['POST', 'GET'])
+def credit():
+    users = client.db.users
+    login_user=users.find_one({'name': session['username']})
+    s1=login_user['credits'] 
+    return render_template("credits.html",s=s1)
+
+@app.route('/book',methods = ['POST', 'GET'])
+def book():
+    india = pytz.timezone('Asia/Kolkata')
+    t1 = datetime.now(india)
+    t2= t1 + timedelta(days=1)
+    t3 = t1 + timedelta(days=2)
+    return render_template("book.html",s1=t1, s2=t2, s3=t3)
+
+@app.route('/confirmbook',methods = ['POST', 'GET'])
+def confirmbook():
+    if request.method=='POST': 
+        slot=(request.form['slot'])
+        day=int(request.form['day'])
+        month=int(request.form['month'])
+        year=int(request.form['year'])
+
+        book = client.db.book
+        
+        status=book.find_one({'slot': slot,'day':day,'month':month,'year':year})
+        if status:
+            s="THE SLOT YOU HAVE PICKED IS ALDREADY BOOKED, PLEASE CHOOSE A DIFFERENT SLOT!"
+            return render_template('confirmbooking.html',s=s)
+        else :
+            india = pytz.timezone('Asia/Kolkata')
+            tnow = datetime.now(india)
+            a= int(tnow.year) <= year
+            b = int(tnow.month) <= month
+            c = int(tnow.day) <= day
+            hour = (int(slot[0]) + 12)%24
+            d = int(tnow.hour) < hour
+
+            if a and b and c and d :
+                temp_obj={"username":session['username'],"slot": slot,"day":day,"month":month,"year":year}
+                temp = client.db.temp
+                temp.insert({'store':temp_obj})
+                return redirect('/ask')
+            else :
+                s="You have booked too late. please choose a different slot!"
+                return render_template('confirmbooking.html',s=s,a=a,b=b,c=c,d=d)
+
+@app.route('/previous_bookings',methods = ['POST', 'GET'])
+def previous_bookings():
+       book=client.db.user_book
+       l=[]
+       for document in book.find():
+            if document['username'] == session['username'] :
+                l.append({'slot': document['slot'],'day':document['day'],'month':document['month'],'year':document['year']})
+
+       return render_template("previous_bookings.html",list=l,s=session['username'])
+
+@app.route('/ask',methods = ['POST', 'GET'])
+def ask():
+       return render_template("ask.html")
+    
+@app.route('/successbook',methods = ['POST', 'GET'])
+def successbook():
+       temp = client.db.temp
+       for l in temp.find():
+           l1=l['store']
+           l2=l1['username']
+           if l2 == session["username"] :
+                temp_obj=l1
+                break
+       client.db.temp.drop()
+       slot=temp_obj["slot"]
+       day=int(temp_obj['day'])
+       month=int(temp_obj['month'])
+       year=int(temp_obj['year'])
+
+       book = client.db.book
+       user_book=client.db.user_book
+       book.insert({'slot': slot,'day':day,'month':month,'year':year}) 
+       user_book.insert({'username':session['username'],'slot': slot,'day':day,'month':month,'year':year}) 
+       updatecredits()
+
+       s= "SUCCESFULLY BOOKED"
+       return render_template('confirmbooking.html',s=s)
+
+def updatecredits():
+    u=client.db.users
+    for i in u.find({'name': session['username']}):
+        temp=i
+        break
+    
+    u.update({"_id" : temp["_id"]},
+    {"credits" : temp["credits"] - 1000,"name":temp["name"],"password":temp["password"],"email":temp["email"],"address":temp["address"]})
+    return 
+
+@app.route('/user_details',methods = ['POST', 'GET'])
+def user_details():
+    u=client.db.users
+    for i in u.find({'name': session['username']}):
+        temp=i
+        break
+    return render_template("user_details.html",t=temp)
 
 
 if __name__ == '__main__' :
